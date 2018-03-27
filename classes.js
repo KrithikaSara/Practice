463e7b05d3343ec7550d06fa8d24b315778891f9
KrithikaSara <34168580+KrithikaSara@users.noreply.github.com>
2018-03-27 10:13:53 +0530

    with getter and setter

--- classes.js
+++ classes.js
@@ -1,3 +1,5 @@
+/*25-mar-2018
+
 function User(name){
     this.name=name;
 }
@@ -18,4 +20,26 @@ class User2{
 }
 
 let user2 = new User2("John");
-user2.sayHi();
+user2.sayHi(); */
+
+/*26-mar-2018*/
+
+class User3{
+	constructor(name){
+		this.name=name;
+	}
+	get name(){
+		return this.name;
+	}
+	set name(value){
+		if(value.length<4){
+			alert("Name is too short.");
+			return;
+		}
+		this.name=value;
+	}
+}
+let user= new user3("John");
+alert(user.name);
+
+user= new User3("");
