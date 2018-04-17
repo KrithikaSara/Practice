a7a48cb28ece1630272cd3ed974f37be82ad54d9
Nivendha <knivendha@gmail.com>
2018-04-17 15:42:52 +0530

    Update mark1.js

---
+++ mark1.js
@@ -0,0 +1,56 @@
+/*
+ PACKMAN design
+=======================
+1) packamn moves all ways, and eats coins
+2) cannot pass though walls
+3) hits ghost it dies
+
+*/
+//================================
+/*
+
+board
+-------
+colour
+points
+isLive
+difficulty
+buildWalls()
+increasePoints()
+start()
+reset()
+
+packman
+------------
+up()
+down()
+right()
+left()
+colour
+speed
+
+ghost
+----------
+colour
+speed
+moveRandom()
+
+*/
+
+//==========================================
+/*
+ return:start(),reset()
+ args: config = {
+		hero,
+		ghost,
+		ghostCount,
+		difficulty
+}
+*/
+funcion Board(){
+
+}
+
+
+
+
