2ff33b1db6ba0644a2e949f3a072227a4458bb6f
KrithikaSara <34168580+KrithikaSara@users.noreply.github.com>
2018-04-18 16:10:11 +0530

    initial_1

---
+++ pacman.js
@@ -0,0 +1,78 @@
+
+
+function Board(){
+
+ var DefaultConfig = { 
+ 		hero,
+		ghost,
+		ghostCount,
+		difficulty
+
+ }
+
+ function increasesPoints(){
+
+ }
+
+ // builds the wall randomly
+ function buildWall(){
+
+ }
+
+	return {
+		start: start,
+		reset:reset	
+	}
+}
+
+// starts the game 
+Board.prototype.start = function(){
+
+}
+
+// when the game is over, resets the board and the score is also reset
+Board.prototype.reset = function(){
+
+}
+
+/*
+return: null
+args: colour,speed
+functionality: constructs packman and moves the packman in all directions
+*/
+function packman{
+	// moves the pacman up till direction is changed and stop if it  hits a wall or ghost
+	function up(){
+
+	}
+
+	// moves the pacman down till direction is changed and stop if it  hits a wall or ghost
+	function down (){
+
+	}
+
+	// moves the pacman left till direction is changed and stop if it  hits a wall or ghost
+	function left(){
+
+	}
+
+	// moves the pacman right till direction is changed and stop if it  hits a wall or ghost
+	function right(){
+
+	}
+}
+
+
+/*
+ return: null
+ args: colour, speed
+ functionality: constructs the ghost and calls the moveRandom()
+*/
+function ghost(){
+
+	// moves the ghost randomly
+	function moveRandom(){
+
+	}
+}
+
